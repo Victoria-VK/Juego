5d9b370b247f68c1d65cde9caedd1d2b0657311a
vicksbuup <victoria.v@alumnos.upm.es>
2024-05-01 17:51:48 +0200

    menú
    cambios de distribución de los botones en el menú y comienzo de una escena nueva para la configuración

--- Info.gd
+++ Info.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,11 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_empezar_pressed():
-	get_tree().change_scene_to_file("res://Scenes/new_game.tscn")
-	
-
-func _on_salir_2_pressed():
-	get_tree().quit()
